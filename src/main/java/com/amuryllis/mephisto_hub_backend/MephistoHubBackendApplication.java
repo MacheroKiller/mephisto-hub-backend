71aec1a5caa0a42c1b729331d742f74b074b44eb
MacheroKiller <stevan1533@gmail.com>
2026-08-15 11:10:27 -0500

    chore: initial Spring Boot 4.1.0 project scaffold (Java 21, Maven)

---
+++ src/main/java/com/amuryllis/mephisto_hub_backend/MephistoHubBackendApplication.java
@@ -0,0 +1,13 @@
+package com.amuryllis.mephisto_hub_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MephistoHubBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MephistoHubBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/amuryllis/mephisto_hub_backend/MephistoHubBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amuryllis.mephisto_hub_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MephistoHubBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
